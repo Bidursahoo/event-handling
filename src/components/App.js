0f3b90ef493a92019dc02eaaeb8c6edea658f3df
Bidursahoo <bibhabsahoobidur@gmail.com>
2023-06-18 21:49:38 +0530

    ..

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
-import React from "react";
-
+import React,{useState} from "react";
+// var t1 = "Submitted";
+// var t2 = "Hello";
 function App() {
+    const [text , setText] = useState("Hello");
+    // const [color , setColor] = useState("white");
+    const [value , setValue] = useState("");
+    function onValue(event) {
+        // if(event.target.value.length > 6){
+        //   setColor("green")
+        // }
+        setValue(event.target.value)
+        // if(text.length > 5){
+        //   setColor("green")
+        // }
+    }
+    function clicks(){
+        // setText(t1);
+        // var t3 = t1;
+        // t1 = t2;
+        // t2 = t3;
+        setText(value)
+    }
+    // function mouseOut(){
+    //     setColor("white")
+    // }
+    // function mouseIn(){
+    //     setColor("black")
+    // }
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{text}</h1>
+      <input onChange={onValue} type="text" placeholder="What's your name?" value={value}/>
+      {/* <button onClick={clicks} onMouseOut={mouseOut} onMouseOver={mouseIn} style={{backgroundColor:color}}>Submit</button> */}
+      <button onClick={clicks} >Submit</button>
     </div>
   );
 }
